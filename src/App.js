f9b268b5b60fdc6045047b2f3cfeb129daca249c
DorottyaB <dorottyabalogh89@gmail.com>
2022-11-25 16:07:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [images, setImages] = useState(data);
+  const [sortedImages, setSortedImages] = useState(images);
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  function sortImages() {
+    setSortedImages(prevState => shuffle(prevState));
+  }
+
+  const [score, setScore] = useState(0);
+  const [isOver, setIsOver] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [message, setMessage] = useState('');
+
+  let bestScore = JSON.parse(localStorage.getItem('score')) || 0;
+
+  useEffect(() => {
+    if (isOver) {
+      if (!localStorage.getItem('score')) {
+        localStorage.setItem('score', JSON.stringify(score));
+      } else {
+        const prevScore = localStorage.getItem('score');
+        if (score > prevScore) {
+          localStorage.setItem('score', JSON.stringify(score));
+          setMessage('New Best Score!');
+        } else {
+          setMessage('You clicked on an image twice!');
+        }
+        setIsVisible(true);
+      }
+      setScore(0);
+      setIsOver(false);
+    }
+  }, [isOver, score]);
+
+  function handleClick(item) {
+    if (item.isClicked) {
+      setIsOver(true);
+      setSortedImages(prevImages =>
+        prevImages.map(image => {
+          return { ...image, isClicked: false };
+        })
+      );
+    } else {
+      setSortedImages(prevImages =>
+        prevImages.map(image => {
+          return image.id === item.id ? { ...image, isClicked: true } : image;
+        })
+      );
+      setScore(prevScore => prevScore + 1);
+      sortImages();
+    }
+    setIsVisible(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        {isVisible && <div className='popup'>{message}</div>}
+        <div>
+          <h1>Memory Game</h1>
+          <p>Get points by clicking on each image once!</p>
+        </div>
+        <div>
+          <h3 className='score'>Score: {score}</h3>
+          <h3 className='best-score'>Best Score: {bestScore}</h3>
+        </div>
       </header>
+      <div className='image-container'>
+        {sortedImages.map(image => (
+          <img
+            src={image.url}
+            alt='illustration'
+            key={image.id}
+            onClick={() => handleClick(image)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    id: 1,
+    isClicked: false,
+    url: './images/teddy-bear.webp',
+  },
+  {
+    id: 2,
+    isClicked: false,
+    url: './images/cat-with-love-sign-hand.webp',
+  },
+  {
+    id: 3,
+    isClicked: false,
+    url: './images/cat-wearing-glasses.webp',
+  },
+  {
+    id: 4,
+    isClicked: false,
+    url: './images/husky-dog-sitting.webp',
+  },
+  {
+    id: 5,
+    isClicked: false,
+    url: './images/hamster-holding-cheek.webp',
+  },
+  {
+    id: 6,
+    isClicked: false,
+    url: './images/panda-with-bamboo.webp',
+  },
+  {
+    id: 7,
+    isClicked: false,
+    url: './images/sloth-yoga.webp',
+  },
+  {
+    id: 8,
+    isClicked: false,
+    url: './images/sloth-sleeping-with-coffee-cup.webp',
+  },
+  {
+    id: 9,
+    isClicked: false,
+    url: './images/zebra-sitting.webp',
+  },
+  {
+    id: 10,
+    isClicked: false,
+    url: './images/cat-playing-yarn-ball.webp',
+  },
+  {
+    id: 11,
+    isClicked: false,
+    url: './images/dog-sitting.webp',
+  },
+  {
+    id: 12,
+    isClicked: false,
+    url: './images/happy-penguin.webp',
+  },
+];
+
+export default data;
